97afec0811d7dd505be6a5817a89ff869d8629e3
Satoshi Fujinaga <b.fujinaga@gmail.com>
2017-07-13 00:54:10 +0900

    fix typo

--- provision_init.sh
+++ provision_init.sh
@@ -58,7 +58,7 @@ fi
 sudo cat <<__END_OF_MESSAGE__ > /etc/profile.d/mecab.sh
 #!/bin/bash
 
-export PATH="$PATH:/usr/local/mecab/bin
+export PATH="$PATH:/usr/local/mecab/bin"
 __END_OF_MESSAGE__
 
 
