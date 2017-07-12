fae191252b08f6fbc97012be57462b603fe04cfa
Satoshi Fujinaga <fujinaga@gnavi.co.jp>
2017-07-12 18:27:46 +0900

    set python env variable

--- provision_init.sh
+++ provision_init.sh
@@ -11,4 +11,13 @@ sudo yum -y upgrade
 sudo yum -y install centos-release-scl-rh
 sudo yum -y install python27
 
+# enable python 2.7
+sudo cat <<__END_OF_MESSAGE__ >/etc/profile.d/python27.sh
+#!/bin/bash
+
+source /opt/rh/python27/enable
+export X_SCLS="`scl enable python27 'echo $X_SCLS'`"
+__END_OF_MESSAGE__
+
+
 
