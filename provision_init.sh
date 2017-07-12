65f87dd3fff26aecf2dac110923d776a79eeb14d
Satoshi Fujinaga <b.fujinaga@gmail.com>
2017-07-12 19:55:54 +0900

    install git & word2vec

--- provision_init.sh
+++ provision_init.sh
@@ -7,6 +7,9 @@
 # yum upgrade
 sudo yum -y upgrade
 
+# install git
+sudo yum -y install git
+
 # install python 2.7
 sudo yum -y install centos-release-scl-rh
 sudo yum -y install python27
@@ -18,6 +21,15 @@ sudo cat <<__END_OF_MESSAGE__ >/etc/profile.d/python27.sh
 source /opt/rh/python27/enable
 export X_SCLS="`scl enable python27 'echo $X_SCLS'`"
 __END_OF_MESSAGE__
+
+# install word2vec
+if [ ! -d /vagrant/word2vec ]
+then
+    cd /vagrant
+    git clone https://github.com/svn2github/word2vec.git
+    cd word2vec
+    make
+fi
 
 
 
