0c7186111b15cf2bcb0aa351926588e5a020705d
Satoshi Fujinaga <fujinaga@gnavi.co.jp>
2017-07-13 16:13:36 +0900

    change install method

--- provision_init.sh
+++ provision_init.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-# relink mount setting
-# @see https://www.virtualbox.org/ticket/16670
-#sudo rm /sbin/mount.vboxsf
-#sudo ln -s /usr/lib/VBoxGuestAdditions/mount.vboxsf /sbin/mount.vboxsf
+
+# set install version
+PYTHON2="2.7.13"
+PYTHON3="3.3.6"
+PYTHON2_FULL="2.7.13"
+PYTHON3_FULL="3.3.6"
+ENSY_INSTALL="0.7.3"
+MECAB="0.996"
+IPADIC="2.7.0-20070801"
 
 # yum upgrade
 sudo yum -y upgrade
@@ -10,17 +15,70 @@ sudo yum -y upgrade
 # install git
 sudo yum -y install git
 
-# install python 2.7
-sudo yum -y install centos-release-scl-rh
-sudo yum -y install python27
+# install dev tools
+sudo yum -y groupinstall "Development tools"
+sudo yum -y install zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel
 
-# enable python 2.7
-sudo cat <<__END_OF_MESSAGE__ > /etc/profile.d/python27.sh
-#!/bin/bash
+# install python 2
+if [ ! -e /opt/local/bin/python${PYTHON2} ]
+then
+    # get python 2
+    cd /usr/local/src
+    curl -O https://www.python.org/ftp/python/${PYTHON2_FULL}/Python-${PYTHON2_FULL}.tgz
+    tar zxf Python-${PYTHON2_FULL}.tgz
 
-source /opt/rh/python27/enable
-export X_SCLS="`scl enable python27 'echo $X_SCLS'`"
-__END_OF_MESSAGE__
+    # install python 2.7.13
+    cd Python-${PYTHON2_FULL}
+    ./configure --prefix=/opt/local
+    make && make altinstall
+fi
+
+# install python3
+if [ ! -e /opt/local/bin/python${PYTHON3} ]
+then
+    # get python3
+    cd /usr/local/src
+    curl -O https://www.python.org/ftp/python/${PYTHON3_FULL}/Python-${PYTHON3_FULL}.tgz
+    tar zxf Python-${PYTHON3_FULL}.tgz
+
+    # install python3
+    cd Python-${PYTHON3_FULL}
+    ./configure --prefix=/opt/local
+    make && make altinstall
+fi
+
+# install Easy_install
+if [ ! -e /usr/local/src/distribute-${ENSY_INSTALL} ]
+then
+    # install Easy_install
+    cd /usr/local/src
+    curl -O https://pypi.python.org/packages/source/d/distribute/distribute-${ENSY_INSTALL}.tar.gz
+    tar xzf distribute-${ENSY_INSTALL}.tar.gz
+    cd distribute-${ENSY_INSTALL}
+    /opt/local/bin/python${PYTHON2} ./distribute_setup.py
+    /opt/local/bin/python${PYTHON3} ./distribute_setup.py
+fi
+
+<< COMMENTOUT
+# install pip2
+if [ ! -e /opt/local/bin/pip${PYTHON2} ]
+then
+    # install pip
+    curl -kL https://bootstrap.pypa.io/get-pip.py | /opt/local/bin/python${PYTHON2}
+
+    #install virtualenv
+    /opt/local/bin/${PYTHON2} install virtualenv
+fi
+
+# install pip3
+if [ ! -e /opt/local/bin/pip${PYTHON3} ]
+then
+    # install pip
+    curl -kL https://bootstrap.pypa.io/get-pip.py | /opt/local/bin/python${PYTHON3}
+
+    #install virtualenv
+    /opt/local/bin/${PYTHON3} install virtualenv
+fi
 
 # install word2vec
 if [ ! -d /vagrant/word2vec ]
@@ -38,20 +96,22 @@ then
 
     sudo mkdir /usr/local/mecab
     cd /usr/local/src
-    wget  wget -O mecab-0.996.tar.gz "https://drive.google.com/uc?export=download&id=0B4y35FiV1wh7cENtOXlicTFaRUE"
-    tar xvfz mecab-0.996.tar.gz
-    cd mecab-0.996
+    wget -O mecab-${MECAB}.tar.gz "https://drive.google.com/uc?export=download&id=0B4y35FiV1wh7cENtOXlicTFaRUE"
+    tar xvfz mecab-${MECAB}.tar.gz
+    cd mecab-${MECAB}
     sudo ./configure --enable-utf8-only --prefix=/usr/local/mecab
     sudo make
     sudo make install
 
     cd /usr/local/src
-    wget -O mecab-ipadic-2.7.0-20070801.tar.gz "https://drive.google.com/uc?export=download&id=0B4y35FiV1wh7MWVlSDBCSXZMTXM"
-    tar xvfz mecab-ipadic-2.7.0-20070801.tar.gz
-    cd mecab-ipadic-2.7.0-20070801
+    wget -O mecab-ipadic-${IPADIC}.tar.gz "https://drive.google.com/uc?export=download&id=0B4y35FiV1wh7MWVlSDBCSXZMTXM"
+    tar xvfz mecab-ipadic-${IPADIC}.tar.gz
+    cd mecab-ipadic-${IPADIC}
     sudo ./configure --prefix=/usr/local/mecab --with-mecab-config=/usr/local/mecab/bin/mecab-config --with-charset=utf8
     sudo make
     sudo make install
+
+    pip install mecab-python
 fi
 
 # enable mecab
@@ -61,6 +121,6 @@ sudo cat <<__END_OF_MESSAGE__ > /etc/profile.d/mecab.sh
 export PATH="$PATH:/usr/local/mecab/bin"
 __END_OF_MESSAGE__
 source /etc/profile.d/mecab.sh
-
+COMMENTOUT
 
 
